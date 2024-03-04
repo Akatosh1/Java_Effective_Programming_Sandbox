4565eb6b7a40b42111e831a36e4f98a4548cb3c5
Akatosh1 <nikitenkov_ea@mail.ru>
2024-03-04 19:48:39 +0500

    initialise

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        var car = new Car("Toyota", 228);
+        System.out.println("Hello, World!" + car.name);
+        
+    }
+}
+
+
---
+++ src/Car.java
@@ -0,0 +1,17 @@
+public class Car {
+    String name;
+    int number;
+
+    public Car(String name, int number){
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+}
